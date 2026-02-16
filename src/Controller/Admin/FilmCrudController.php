fb13a45d0c426d0b7103deee67935f0db185c757
juliettedef <juliette.defrance@outlook.fr>
2026-02-16 11:45:11 +0100

    correction div film

--- src/Controller/Admin/FilmCrudController.php
+++ src/Controller/Admin/FilmCrudController.php
@@ -5,8 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Film;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class FilmCrudController extends AbstractCrudController
 {
@@ -15,14 +15,12 @@ class FilmCrudController extends AbstractCrudController
         return Film::class;
     }
 
-    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('synopsis'),
+            TextareaField::new('synopsis'),
         ];
     }
-    
 }
